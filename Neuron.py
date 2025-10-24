be6226c10603764ab86ed75ab00482501b2b91f6
AchrafLarbi <larbiachraf03@gmail.com>
2025-10-24 14:45:00 +0100

    Add spike dynamics to Neuron class

--- Neuron.py
+++ Neuron.py
@@ -5,3 +5,11 @@ class Neuron:
     def __init__(self, input_size):
         self.weights = np.random.randn(input_size) * 0.1
         self.bias = 0.0
+
+    def forward(self, inputs):
+        """Compute membrane potential"""
+        return np.dot(inputs, self.weights) + self.bias
+    
+    def spike(self, potential, threshold=1.0):
+        """Check if neuron spikes"""
+        return potential > threshold
