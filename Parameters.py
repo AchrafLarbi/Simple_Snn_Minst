a9e2c8d438e2fef0e453d3ae456a7c9f8a9d8304
AchrafLarbi <larbiachraf03@gmail.com>
2025-10-27 10:30:00 +0100

    Add STDP parameters to configuration

--- Parameters.py
+++ Parameters.py
@@ -8,3 +8,8 @@ class Parameters:
     time_steps = argparse.IntRange(10, 1000, '100')
     batch_size = argparse.IntRange(1, 256, '32')
     epochs = argparse.IntRange(1, 100, '10')
+
+    # STDP parameters
+    tau_plus = argparse.FloatRange(1.0, 100.0, '20.0')
+    tau_minus = argparse.FloatRange(1.0, 100.0, '20.0')
+    stdp_learning_rate = argparse.FloatRange(0.00001, 0.01, '0.0001')
